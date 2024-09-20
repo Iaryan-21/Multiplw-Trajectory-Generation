4590a5ad7bf50d8454d85faff82de2bf7aa9d410
Aryan Mishra <86413005+Iaryan-21@users.noreply.github.com>
2024-09-20 17:51:15 -0400

    Update cores.py

--- cores.py
+++ cores.py
@@ -1,9 +1,6 @@
 import psutil
 
-# Get the number of logical cores
 logical_cores = psutil.cpu_count(logical=True)
-
-# Get the number of physical cores
 physical_cores = psutil.cpu_count(logical=False)
 
 print(f"Logical cores: {logical_cores}")
